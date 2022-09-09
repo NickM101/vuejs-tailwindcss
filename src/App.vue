b7d7390ae722729965432ad58f232b9322c8a751
Nick Munene <nickmunene101@gmail.com>
2022-09-09 07:26:01 +0300

    Working on Main section

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,37 @@
-<script setup></script>
+<script setup>
+import Carousel from './components/Carousel.vue'
+import Banner from './components/Banner.vue'
+import Container from './components/Container.vue'
+</script>
 
 <template>
-  <h1 class="text-3xl font-bold">Hello world!</h1>
+  <div class="flex flex-col min-h-screen">
+    <header class="bg-indigo-600 h-14">
+      <nav class="flex justify-between items-center px-4 py-2">
+        <img src="./assets/images/ho_me.png" />
+
+        <img src="./assets/images/ho_lo.png" />
+
+        <img src="./assets/images/ho_li.png" />
+      </nav>
+    </header>
+    <main class="bg-amber-500 px-3">
+      <banner />
+      <carousel />
+      <container />
+    </main>
+    <footer class="bg-red-900 h-14 sticky top-[100vh]">
+      <nav class="flex justify-between px-4 py-4">
+        <img src="./assets/images/ho_ho1.png" />
+
+        <img src="./assets/images/ho_ac2.png" />
+
+        <img src="./assets/images/ho_wa2.png" />
+        <img src="./assets/images/ho_co2.png" />
+        <img src="./assets/images/ho_my2.png" />
+      </nav>
+    </footer>
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/Carousel.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+const hot_games = ref([1, 2, 3, 4, 5, 6, 7, 8, 9])
+</script>
+
+<template>
+  <section>
+    <div class="my-4"><p class="text-bold text-amber-400">Hot Game</p></div>
+    <div
+      class="flex items-center gap-5 flex-grow-0 overflow-auto md:overflow-scroll"
+    >
+      <div
+        class="flex justify-center items-center h-20 w-20 rounded-md bg-slate-200"
+        v-for="(game, index) in hot_games"
+        :key="game"
+      >
+        <p>{{ game }}</p>
+      </div>
+    </div>
+  </section>
+</template>
